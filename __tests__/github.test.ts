891d7a74088de234f2a5e74334308530edbb3ac8
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-07 13:16:41 +0200

    Typo

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -8,7 +8,7 @@ describe('github', () => {
     expect(release?.tag_name).not.toEqual('');
   });
 
-  it('returns v1.8.0 GoReleaser GitHub release', async () => {
+  it('returns v1.8.0 Mage GitHub release', async () => {
     const release = await github.getRelease('v1.8.0');
     console.log(release);
     expect(release).not.toBeNull();
