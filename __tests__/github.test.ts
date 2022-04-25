260e40f2a81e9f496c52ea575870a7f9e3d06376
CrazyMax <crazy-max@users.noreply.github.com>
2022-04-25 04:46:32 +0200

    update dev dependencies and workflow

--- __tests__/github.test.ts
+++ __tests__/github.test.ts
@@ -1,16 +1,15 @@
+import {describe, expect, it} from '@jest/globals';
 import * as github from '../src/github';
 
 describe('github', () => {
   it('returns latest Mage GitHub release', async () => {
     const release = await github.getRelease('latest');
-    console.log(release);
     expect(release).not.toBeNull();
     expect(release?.tag_name).not.toEqual('');
   });
 
   it('returns v1.8.0 Mage GitHub release', async () => {
     const release = await github.getRelease('v1.8.0');
-    console.log(release);
     expect(release).not.toBeNull();
     expect(release?.tag_name).toEqual('v1.8.0');
   });
--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -1,16 +1,15 @@
-import fs = require('fs');
+import {describe, expect, it} from '@jest/globals';
+import * as fs from 'fs';
 import * as installer from '../src/installer';
 
 describe('installer', () => {
   it('acquires v1.8.0 version of Mage', async () => {
     const mage = await installer.getMage('v1.8.0');
-    console.log(mage);
     expect(fs.existsSync(mage)).toBe(true);
   }, 100000);
 
   it('acquires latest version of Mage', async () => {
     const mage = await installer.getMage('latest');
-    console.log(mage);
     expect(fs.existsSync(mage)).toBe(true);
   }, 100000);
 });
