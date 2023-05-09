b8b6e3da198938d1f6f35cd9edb66a4aca270817
Dmitry Volodin <dmvolod@gmail.com>
2023-05-09 10:00:36 +0300

    Add local cache and actions/cache support
    Co-authored-by: CrazyMax <github@crazymax.dev>
    
    Signed-off-by: Dmitry Volodin <dmvolod@gmail.com>

--- __tests__/installer.test.ts
+++ __tests__/installer.test.ts
@@ -32,6 +32,11 @@ describe('installer', () => {
     expect(fs.existsSync(mage)).toBe(true);
   }, 100000);
 
+  it('acquires v1.8.0 version of Mage from local cache', async () => {
+    const mage = await installer.getMage('v1.8.0');
+    expect(fs.existsSync(mage)).toBe(true);
+  }, 100000);
+
   it('acquires latest version of Mage', async () => {
     const mage = await installer.getMage('latest');
     expect(fs.existsSync(mage)).toBe(true);
